c5d2ec4fad71f6b36bbca4c2013e5ca49a41df62
Vikas Gupta <vikasgpt76@gmail.com>
2020-08-14 17:09:52 +0530

    Rajasthan

--- Scrappers/Rajasthan_Scrapper.py
+++ Scrappers/Rajasthan_Scrapper.py
@@ -20,61 +20,109 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 import time
+import sys
+sys.path.append(r'E:\D_Drive\Scrapping\Causelist_Project')
 from selenium.webdriver.common.keys import Keys
+from Segmentation.Segmentation_code import parseFiles
+from Parsers.PDF_Parser_Rajasthan import parsepdf
 
-today_date = datetime.today().strftime('%d-%m-%Y')
-print(today_date)
 
-chrome_options = Options()
-download_dir = r'D:\Scrapping\Rajasthan\PDF'
-try:
-    os.makedirs(download_dir)
-except:
-    pass
+def scrapper(url):
+    today_date = datetime.today().strftime('%d-%m-%Y')
+    print(today_date)
+    chrome_options = Options()
+    download_dir = r'E:\Scrapping\Rajasthan\PDF' # + '\/'+  today_date 
+    try:
+        os.makedirs(download_dir)
+    except:
+        pass
+    
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {
+    "download.default_directory": download_dir,  # Change default directory for downloads
+    "download.prompt_for_download": False,
+    "plugins.always_open_pdf_externally": True  # To auto download the file
+    }) 
+    
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+    command_result = driver.execute("send_command", params)
+    
+    driver.get(url)
+    
+    Date_Selection = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/table/tbody/tr[3]/td/input[1]")
+    
+    Date_Selection.send_keys(today_date)
+    
+    go_button = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/table/tbody/tr[3]/td/input[2]")
+    
+    go_button.click()
+    
+    Causelist_table = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/div[2]/table")
+    
+    pdf_links = []
+    
+    for pdf_element in Causelist_table.find_elements_by_xpath(".//a"):
+        time.sleep(2)
+        if (pdf_element.text == "View"):
+            pdf_link = pdf_element.get_attribute("href")
+            pdf_links.append(pdf_link)
+            
+    
+    Count = 1
+    for link in pdf_links:
+       print (link)
+       try:
+           driver.get(link)
+           time.sleep(8)
+           os.rename(os.path.join(download_dir, "display_causelist.pdf"), os.path.join(download_dir, "display_causelist_" + str(Count) + ".pdf"))
+           Count += 1
+       except:
+           pass
+    return download_dir
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('prefs', {
-"download.default_directory": download_dir,  # Change default directory for downloads
-"download.prompt_for_download": False,
-"plugins.always_open_pdf_externally": True  # To auto download the file
-}) 
+list1 = []
 
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-command_result = driver.execute("send_command", params)
-
+list2 = []
+    
 url = "https://services.ecourts.gov.in/ecourtindiaHC/cases/highcourt_causelist.php?state_cd=9&dist_cd=1&court_code=1&stateNm=Rajasthan"
 
-driver.get(url)
 
-Date_Selection = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/table/tbody/tr[3]/td/input[1]")
+def PDFLocation(pathofPDF):
+    for dirpath, dirname, filenames in os.walk(pathofPDF):
+        for file in filenames:
+            try:
+                if file.lower().endswith(".pdf"):
+                    path = os.path.abspath(os.path.join(dirpath, file))
+                    list1.append(path)
+                else:
+                    continue
+            except (FileNotFoundError, IOError):
+                print("runtime exception")
+    return list1
 
-Date_Selection.send_keys(input("Enter the date in dd-mm-yyyy format"))
+def jsonread(pathofjson):
+    for dirpath, dirname, filenames in os.walk(pathofjson):
+        for file in filenames:
+            try:
+                if file.lower().endswith(".json"):
+                    path = os.path.abspath(os.path.join(dirpath, file))
+                    list2.append(path)
+                else:
+                    continue
+            except (FileNotFoundError, IOError):
+                print("runtime exception")
+    return list2
 
-go_button = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/table/tbody/tr[3]/td/input[2]")
+download_dir = scrapper(url)
 
-go_button.click()
+time.sleep(15)
 
-Causelist_table = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/div[2]/table")
+pdf_list = PDFLocation(download_dir)
 
-pdf_links = []
+json_list = jsonread(download_dir)
 
-for pdf_element in Causelist_table.find_elements_by_xpath(".//a"):
-    time.sleep(2)
-    if (pdf_element.text == "View"):
-        pdf_link = pdf_element.get_attribute("href")
-        pdf_links.append(pdf_link)
-        
-
-Count = 1
-for link in pdf_links:
-   print (link)
-   try:
-       driver.get(link)
-       time.sleep(8)
-       os.rename(os.path.join(download_dir, "display_causelist.pdf"), os.path.join(download_dir, "display_causelist_" + str(Count) + ".pdf"))
-       Count += 1
-   except:
-       pass
+for file in pdf_list:
+    parsepdf(parseFiles(file, download_dir), download_dir)
