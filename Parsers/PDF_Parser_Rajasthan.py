18cfb7b1ea0cac34d168e49654d3ff341bb0424e
Vikas Gupta <vikasgpt76@gmail.com>
2020-08-14 16:52:06 +0530

    Rajasthan

---
+++ Parsers/PDF_Parser_Rajasthan.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 14:54:09 2020
+
+@author: Vikas Gupta
+"""
+
+
+import re
+
+
+regexp = re.compile(r'^([0-9]{1,2}\n)')
+case_regex = re.compile(r'((CRP\(C.R.P.)|RES.APPL.|I.T. APPEAL|CRL.M.\(BAIL\)|((of|OF|\/|-)(\s)?[0-9]{4})(\s)?(\([-a-zA-Z0-9\s]+\)$)?)')
+case_regex_2nd = re.compile(r'^([0-9]).+?(((of|OF|\/|-)(\s)?[0-9]{4})(\s)?(\([-a-zA-Z0-9\s]+\)$)?)')
+date_regex_acceptance = re.compile(r'(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|January|Feburary|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|AUG|SEPT|OCT|NOV|DEC)(\s+)?(,)?(\d{1,2})?(st|nd|th|rd|TH)?(,)?(\s+)?(\d{4})(\.)?|(\d{1,2})(st|nd|th|rd)?(\s+|-)?(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|January|Feburary|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|AUG|SEPT|OCT|NOV|DEC)(\s+|-)?(,)?(\s+)?(\d{4})(\.)?')
+date_regex = re.compile(r'(((\d{1,2}(-|\/|\.)\d{1,2}(-|\/|\.)(19|20)?\d{2}))(\.)?)|(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|January|Feburary|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|AUG|SEPT|OCT|NOV|DEC)(\s+)?(,)?(\d{1,2})?(st|nd|th|rd|TH)?(,)?(\s+)?(\d{4})(\.)?|(\d{1,2})(st|nd|th|rd)?(\s+|-)?(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|January|Feburary|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|AUG|SEPT|OCT|NOV|DEC)(\s+|-)?(,)?(\s+)?(\d{4})(\.)?')
+            
+
+def parsepdf(data, download_dir):
+        Case_Num = []
+        Case_Numbers = []
+        Party = []
+        Jud_Name = []
+        Batches = []
+        Fault_Files = []
+        JSON_Complete_Data = []
+        for value in range(len(data)):
+            if (regexp.search(data[value-1]['Line_Data']['Value']) and len(data[value]['Line_Data']['Value']) < 60 and case_regex.search(data[value]['Line_Data']['Value']) and not(date_regex.search(data[value]['Line_Data']['Value'])) and "on appeal" not in data[value]['Line_Data']['Value'].lower() and "on an intended appeal" not in data[value]['Line_Data']['Value'].lower() and "file" not in data[value]['Line_Data']['Value'].lower() and "listed" not in data[value]['Line_Data']['Value'].lower() and "in" not in data[value]['Line_Data']['Value'].lower() and "with" not in data[value]['Line_Data']['Value'].lower() and "&" not in data[value]['Line_Data']['Value'].lower() and " pm" not in data[value]['Line_Data']['Value'].lower() and " am" not in data[value]['Line_Data']['Value'].lower()):
+                Case_Details = {"Value" : data[value]['Line_Data']['Value'], "Index": value, "Left_Point" : data[value]['Line_Data']['leftpoint_x'], "Left_Point_Y" : data[value]['Line_Data']['leftpoint_y']}
+                Case_Numbers.append(Case_Details)
+            # elif (case_regex_2nd.search(data[value]['Line_Data']['Value']) and len(data[value]['Line_Data']['Value']) < 60 and not(date_regex.search(data[value]['Line_Data']['Value'])) and "on appeal" not in data[value]['Line_Data']['Value'].lower() and "on an intended appeal" not in data[value]['Line_Data']['Value'].lower() and "file" not in data[value]['Line_Data']['Value'].lower() and "listed" not in data[value]['Line_Data']['Value'].lower() and "in" not in data[value]['Line_Data']['Value'].lower() and "with" not in data[value]['Line_Data']['Value'].lower() and "&" not in data[value]['Line_Data']['Value'].lower() and " pm" not in data[value]['Line_Data']['Value'].lower() and " am" not in data[value]['Line_Data']['Value'].lower()):
+            #     Case_Details = {"Value" : data[value]['Line_Data']['Value'], "Index": value, "Left_Point" : data[value]['Line_Data']['leftpoint_x'], "Left_Point_Y" : data[value]['Line_Data']['leftpoint_y']}
+            #     Case_Numbers.append(Case_Details)
+        for values in range(len(Case_Numbers)):
+            if (values < len(Case_Numbers) -1):
+                Index_1 = Case_Numbers[values]['Index']
+                Index_2 = Case_Numbers[values+1]['Index']
+                batch = []
+                Batch = []
+                Adv_Name = []
+                Case_Numb = []
+                Datelist = []
+                Left_point = 0
+                advocate_names_all_petitioner = []
+                advocate_names_all_respondent = []
+                for index in range(len(data)):
+                    if (index >= Index_1 and index < Index_2):
+                        if (case_regex.search(data[index]['Line_Data']['Value']) or case_regex_2nd.search(data[index]['Line_Data']['Value'])) and not(date_regex.search(data[index]['Line_Data']['Value'])) and "file" not in data[index]['Line_Data']['Value'] and data[index]['Line_Data']['leftpoint_x'] < 160:
+                            Case_Numb.append(data[index]['Line_Data']['Value'].strip())
+                        else:
+                            pass
+                        if (date_regex_acceptance.search(data[index]['Line_Data']['Value'])):
+                            for date in date_regex_acceptance.finditer(data[index]['Line_Data']['Value']):
+                                Datelist.append(date.group())
+                        else:
+                            pass
+                    if (index > Index_1 and index < Index_2) and "VKJNGT+CourierNew,Bold" not in data[index]['Line_Data']['font_name'] and len(data[index]['Line_Data']['Value']) < 70 and not(date_regex.search(data[index]['Line_Data']['Value'])) and data[index]['Line_Data']['leftpoint_x'] > Case_Numbers[values]['Left_Point'] and data[index]['Line_Data']['leftpoint_x'] > 100 and "HON'BLE" not in data[index]['Line_Data']['Value'] and "file" not in data[index]['Line_Data']['Value'] and "admission" not in data[index]['Line_Data']['Value'].lower() and "hearing" not in data[index]['Line_Data']['Value'].lower() and "order" not in data[index]['Line_Data']['Value'].lower() and "part" not in data[index]['Line_Data']['Value'].lower() and " pm" not in data[index]['Line_Data']['Value'].lower() and "bench" not in data[index]['Line_Data']['Value'].lower() and "name of advocate" not in data[index]['Line_Data']['Value'].lower():
+                        batch.append(data[index])
+                    else:
+                        continue
+                Batch = sorted(batch, key=
+                                       lambda x: (round(x['Line_Data']['leftpoint_x'], 0)))
+                Count = 0
+                Party_Name = []
+                Judge_Name = []
+                for ind in range(len(Batch)):
+                    if (ind < len(Batch) -1):
+                        if (Count == 0):
+                            if (Batch[ind]['Line_Data']['leftpoint_x'] > 100 and Batch[ind]['Line_Data']['leftpoint_x'] == Batch[ind+1]['Line_Data']['leftpoint_x']) or (Batch[ind+1]['Line_Data']['leftpoint_x'] - Batch[ind]['Line_Data']['leftpoint_x']) < 5:
+                                Party_Name.append(Batch[ind]['Line_Data']['Value'])
+                                Left_point = Batch[ind]['Line_Data']['leftpoint_x'] 
+                            else:
+                                Count+=1
+                                if(Batch[ind]['Line_Data']['leftpoint_x'] > 100 and Batch[ind]['Line_Data']['Value'] not in Party_Name and Batch[ind]['Line_Data']['leftpoint_x'] == Left_point and Left_point != 0):
+                                    Party_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                                else:
+                                    Party_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                        elif (Count == 1):
+                            if ((Batch[ind]['Line_Data']['leftpoint_x'] == Batch[ind+1]['Line_Data']['leftpoint_x']) or (Batch[ind+1]['Line_Data']['leftpoint_x'] - Batch[ind]['Line_Data']['leftpoint_x']) < 5):
+                                Judge_Name.append(Batch[ind]['Line_Data']['Value'])
+                                Left_point = Batch[ind]['Line_Data']['leftpoint_x']
+                            else:
+                                Count+=1
+                                if(Batch[ind]['Line_Data']['Value'] not in Judge_Name) and 400 > Batch[ind]['Line_Data']['leftpoint_x'] > 300:
+                                        Judge_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                                else:
+                                    pass
+                        elif (Count == 2):
+                            if (Batch[ind]['Line_Data']['leftpoint_x'] == Batch[ind+1]['Line_Data']['leftpoint_x']):
+                                Adv_Name.append(Batch[ind]['Line_Data']['Value'])
+                                Left_point = Batch[ind]['Line_Data']['leftpoint_x'] 
+                            else:
+                                Count+=1
+                    elif (ind == len(Batch) - 1) and 200 > Batch[ind]['Line_Data']['leftpoint_x'] > 130:
+                        Party_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                    elif (ind == len(Batch) - 1) and 350 > Batch[ind]['Line_Data']['leftpoint_x'] > 290:
+                        Judge_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                    elif (ind == len(Batch) - 1) and 500 > Batch[ind]['Line_Data']['leftpoint_x'] > 400:
+                        Adv_Name.append(Batch[ind]['Line_Data']['Value'].replace("\xa0",""))
+                if len(Case_Numb) != 0:
+                    if (len(Party_Name) == 0):
+                        Fault_Files.append("For the case_number {}, the Party name is not captured".format(Case_Numb[0]))
+                    if (len(Judge_Name) == 0):
+                        Fault_Files.append("For the case_number {}, the Advocate name is not captured".format(Case_Numb[0]))
+                else:
+                    pass
+                Cleaned_Case_Numbers = ", ".join(Case_Numb)
+                JSON_Data = {"case_numbers" : Cleaned_Case_Numbers}
+                Cleaned_Party = " ".join(Party_Name)
+                JSON_Data["party_name"] = Cleaned_Party.replace("\n", "")
+                Cleaned_Judge = " ".join(Judge_Name)
+                #JSON_Data["petitioner_advocate_name"] = Cleaned_Judge.replace("\n", "")         
+                Cleaned_Adv_Name = " ".join(Adv_Name)
+                #JSON_Data["respondent_advocate_name"] = Cleaned_Adv_Name.replace("\n", "")
+                for name in Cleaned_Judge.replace("\n", "").split("-"):
+                    if (name == "P" or name.strip() == ""):
+                        pass
+                    else:
+                        advocate_name = {"name": name.replace(" P", "").replace("P ", "")}
+                        advocate_names_all_petitioner.append(advocate_name)
+                JSON_Data["petitioner_advocate_name"] = advocate_names_all_petitioner
+                for name in Cleaned_Adv_Name.replace("\n", "").split("-"):
+                    if (name == "R" or name.strip() == ""):
+                        pass
+                    else:
+                        advocate_name = {"name": name.replace(" R", "").replace("R ", "")}
+                        advocate_names_all_respondent.append(advocate_name)
+                JSON_Data["respondent_advocate_name"] = advocate_names_all_respondent
+                Party.append(Cleaned_Party.replace("\n",""))
+                Jud_Name.append(Cleaned_Judge.replace("\n",""))
+                Case_Num.append(Cleaned_Case_Numbers)
+                JSON_Complete_Data.append(JSON_Data)
+                Batches.append(Batch)
+        print (JSON_Complete_Data)
+        # for value in range(len(data)):
+        #     for case in range(len(Case_Numbers) -1):
+        #         if (value > Case_Numbers[case]['Index'] and value < Case_Numbers[case+1]['Index']):
+        #             if (case_regex.search(data[value]['Line_Data']['Value'])):
+        #                 Case_Num.append(data[value]['Line_Data']['Value'])
+#         with open(Outputlocation + "/" + file_name + "_Batches.txt" ,"w") as f:
+#            for value in Batches:
+#                f.write(str(value))
+#         with open(Outputlocation + "/" + file_name + "_Error_logs.txt" ,"w") as f:
+#            for value in Fault_Files:
+#                f.write(str(value) + "\n")
+
+
+#         df = pd.DataFrame({'Case_Number':Case_Num})
+#         df1 = pd.DataFrame({'Party_Names':Party})
+#         df2 = pd.DataFrame({'Advocate_Names': Jud_Name})
+#         #df3 = pd.DataFrame({'Listing_Date': Date_List})
+#         df3 = pd.DataFrame({'Respondent_Adv_Name': Advocate_Names_Respondent})
+#         writer = pd.ExcelWriter(Outputlocation + "/" +file_name + ".xlsx")
+#         df.to_excel(writer, sheet_name='Sheet1',index=False,startcol=0)
+#         df1.to_excel(writer, sheet_name='Sheet1',index=False,startcol=1)                
+#         df2.to_excel(writer, sheet_name='Sheet1',index=False,startcol=2)
+#         df3.to_excel(writer, sheet_name='Sheet1',index=False,startcol=3)
+#         writer.save()
+
+# inputlocation=input("Please enter the location of JSON : \n")
+# Outputlocation=input("Please enter the location of XML : \n")
+# list1=[]
+# def readjson(pathofBundle):
+#     for dirpath,dirname,filenames in os.walk(pathofBundle):
+#         for file in filenames:
+#             try:
+#                 if(file.endswith('.json')):
+#                     path=(os.path.abspath(os.path.join(dirpath,file)))
+#                     list1.append(path)
+#                 else:
+#                     continue
+#             except (FileNotFoundError, IOError):
+#                 print("runtime exception")
+#     return list1
+# readjson(inputlocation)
+# parsefiles(list1)
